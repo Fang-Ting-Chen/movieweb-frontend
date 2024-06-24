64006521d6f656a658c107816c21fee53b5781e0
Fang-Ting Chen <chenf891127@gmail.com>
2024-06-24 16:09:35 +0800

    change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,31 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteImages from 'vite-plugin-vue-images';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
+    ViteImages({
+      dirs: ['src/assets/images', 'src/assets/css'],
+      extensions: ['jpg', 'jpeg', 'png', 'svg', 'webp'],
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: 'src/assets/images/[name].[ext]',
+      }
+    }
   }
-})
+
+});
